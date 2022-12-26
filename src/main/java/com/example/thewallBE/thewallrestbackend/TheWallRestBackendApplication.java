3216f9988385b59671dc6f68317493a162479ab0
Ivan Christian Jay Funcion <icjfuncion@gmail.com>
2022-12-26 23:35:56 +0800

    initial commit for wall backend

---
+++ src/main/java/com/example/thewallBE/thewallrestbackend/TheWallRestBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.thewallBE.thewallrestbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheWallRestBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheWallRestBackendApplication.class, args);
+	}
+
+}
